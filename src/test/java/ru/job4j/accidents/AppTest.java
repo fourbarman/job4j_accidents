5302b7ff0e2bd741f0c711e9340734ed17520f1d
fourbarman <maks.java@yandex.ru>
2023-03-07 12:15:10 +0400

    Accidents. Init new repository.

---
+++ src/test/java/ru/job4j/accidents/AppTest.java
@@ -0,0 +1,18 @@
+package ru.job4j.accidents;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
